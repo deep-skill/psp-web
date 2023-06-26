b1bda68f60ba8f927db1e48d782e967bfaffd8e7
MaxiV95 <maximilianovanmegroot@gmail.com>
2023-06-26 13:17:52 -0300

    initial

---
+++ src/components/landing/Landing.jsx
@@ -0,0 +1,11 @@
+import style from "./Landing.module.css";
+
+const Landing = () => {
+	return (
+		<div className={style.landingContainer}>
+			Hello World!
+		</div>
+	);
+};
+
+export default Landing;
