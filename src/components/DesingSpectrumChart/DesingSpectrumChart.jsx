6c2c7d21a6a0e176ad77f8a31645c8b395a82d8d
josejaramillo098@gmail.com <josejaramillo098@gmail.com>
2023-06-29 13:27:44 -0300

    Components for Charts defined

---
+++ src/components/DesingSpectrumChart/DesingSpectrumChart.jsx
@@ -0,0 +1,9 @@
+const DesingSpectrumChart = () => {
+  return (
+    <>
+      <h1>Desing Spectrum</h1>
+    </>
+  );
+};
+
+export default DesingSpectrumChart;
---
+++ src/components/ExceedanceProbabilityChart/ExceedanceProbabilityChart.jsx
@@ -0,0 +1,9 @@
+const ExceedanceProbabilityChart = () => {
+  return (
+    <>
+      <h1>ExceedanceProbabilityChart</h1>
+    </>
+  );
+};
+
+export default ExceedanceProbabilityChart;
