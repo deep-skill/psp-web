b535c6b42f53546a19871bdb6d884eca7e627fdb
josejaramillo098@gmail.com <josejaramillo098@gmail.com>
2023-07-03 14:22:49 -0300

    Charts components defined

---
+++ src/components/chartsComponents/desingSpectrumChart/DesignSpectrumChart.jsx
@@ -0,0 +1,15 @@
+import { useEffect, useState } from "react";
+import Highcharts from "highcharts";
+import HighchartsReact from "highcharts-react-official";
+import defaultOptions  from "./optionsDesignSpectrumChart";
+
+const DesignSpectrumChart = () => {
+    const [ options, setOptions ] = useState(defaultOptions);
+  return (
+    <>
+      <HighchartsReact highcharts={Highcharts} options={options} />
+    </>
+  );
+};
+
+export default DesignSpectrumChart;
