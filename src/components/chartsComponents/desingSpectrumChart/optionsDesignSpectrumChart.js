0e7b58a21503ac737e41b1752f59f3ac7c931c6c
josejaramillo098@gmail.com <josejaramillo098@gmail.com>
2023-07-05 18:27:45 -0300

    feature to UniformHazard.jsx styles & Chart

--- src/components/chartsComponents/desingSpectrumChart/optionsDesignSpectrumChart.js
+++ src/components/chartsComponents/desingSpectrumChart/optionsDesignSpectrumChart.js
@@ -1,61 +1,80 @@
 const options = {
-    chart: {
-        renderTo: 'container3',
-        type: 'line',
-    },
+  chart: {
+    spacing: [30, 10, 5, 10],
+    backgroundColor: "#003459",
+    borderRadius: 5,
+    type: "line",
+  },
+  title: {
+    text: null,
+    x: -20,
+  },
+  xAxis: {
     title: {
-        text: null,
-        x: -20 
+      text: "Periodo estructural (s)",
+      style: {
+        color: "#fff",
+      },
     },
-    xAxis: {
-        title: {
-            text: 'Periodo estructural (s)'
-        },
-        gridLineWidth: 1,
-        gridZIndex: 4,
-        labels: {
-            formatter: function() {
-                return this.value.toFixed(2);
-            }
-        }
+    lineColor: "#fff",
+    gridLineWidth: 1,
+    gridZIndex: 4,
+    labels: {
+      formatter: function () {
+        return this.value.toFixed(2);
+      },
     },
-    yAxis: {
-        title: {
-            text: 'Aceleración espectral (g)'
-        },
-        plotLines: [{
-            value: 0,
-            width: 1,
-            color: '#808080'
-        }],
-        labels: {
-            formatter: function() {
-                return this.value.toFixed(2);
-            }
-        }
+  },
+  yAxis: {
+    title: {
+      text: "Aceleración espectral (g)",
+      style: {
+        color: "#fff",
+      },
     },
-    legend: {
-        layout: 'vertical',
-        align: 'right',
-        verticalAlign: 'middle',
-        borderWidth: 0
+    plotLines: [
+      {
+        value: 0,
+        width: 1,
+        color: "#fff",
+      },
+    ],
+    labels: {
+      formatter: function () {
+        return this.value.toFixed(2);
+      },
+      style: {
+        color: "#fff",
+      },
     },
-    plotOptions: {
-        series: {
-            marker: {
-                enabled: false
-            }
-        }
+  },
+  legend: {
+    layout: "vertical",
+    align: "center",
+    verticalAlign: "bottom",
+    borderWidth: 0,
+    itemStyle: {
+      color: "#fff",
     },
-    series: [{
-        name: '',
-        data: []
-    }],
-    tooltip: {
-        formatter: function() {
-            return '(' + this.x.toFixed(3) + ', ' + this.y.toFixed(3) + ')';
-        }
-    }
+  },
+  plotOptions: {
+    series: {
+      marker: {
+        enabled: false,
+      },
+    },
+  },
+  series: [
+    {
+      name: "",
+      data: [],
+    },
+  ],
+  tooltip: {
+    formatter: function () {
+      return "(" + this.x.toFixed(3) + ", " + this.y.toFixed(3) + ")";
+    },
+  },
 };
 
 export default options;
--- src/components/uniformHazard/UniformHazard.jsx
+++ src/components/uniformHazard/UniformHazard.jsx
@@ -1,93 +1,95 @@
 import style from "./UniformHazard.module.css";
 import { useDispatch, useSelector } from "react-redux";
 import {
-	selectNorm,
-	selectSoil,
-	inputReturnPeriod,
-	selectReturnPeriod,
+  selectNorm,
+  selectSoil,
+  inputReturnPeriod,
+  selectReturnPeriod,
 } from "../../redux/actions";
 import { useEffect, useState } from "react";
 import { getStandards } from "../../Helpers/backendRequests/requestToStandards";
-import DesignSpectrumChart from '../chartsComponents/desingSpectrumChart/DesignSpectrumChart'
+import DesignSpectrumChart from "../chartsComponents/desingSpectrumChart/DesignSpectrumChart";
 
 const UniformHazard = () => {
-	const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-	const onSearch = () => {
-		dispatch(inputReturnPeriod(0))
-	};
-	const returnPeriodInputted = useSelector(
-		(state) => state.slice.returnPeriodInputted
-	);
+  const onSearch = () => {
+    dispatch(inputReturnPeriod(0));
+  };
+  const returnPeriodInputted = useSelector(
+    (state) => state.slice.returnPeriodInputted
+  );
 
-	const buttonsReturnPeriod = [475, 1000, 2475];
-	const returnPeriodSelected = useSelector(
-		(state) => state.slice.returnPeriodSelected
-	);
+  const buttonsReturnPeriod = [475, 1000, 2475];
+  const returnPeriodSelected = useSelector(
+    (state) => state.slice.returnPeriodSelected
+  );
 
-	const norms = useSelector((state) => state.slice.norms);
-	const soils = useSelector((state) => state.slice.soils);
-	const normSelected = useSelector((state) => state.slice.normSelected);
-	const soilSelected = useSelector((state) => state.slice.soilSelected);
+  const norms = useSelector((state) => state.slice.norms);
+  const soils = useSelector((state) => state.slice.soils);
+  const normSelected = useSelector((state) => state.slice.normSelected);
+  const soilSelected = useSelector((state) => state.slice.soilSelected);
 
-	const handleChange = (event) => {
-		const { name, value } = event.target;
-		const action = { selectNorm, selectSoil };
-		dispatch(action[name](value));
-	};
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    const action = { selectNorm, selectSoil };
+    dispatch(action[name](value));
+  };
 
-	useEffect(() => {
-		!norms && dispatch(getStandards());
-	}, []);
+  useEffect(() => {
+    !norms && dispatch(getStandards());
+  }, []);
 
-	return (
-		<div className={style.probabilityContainer}>
-			<h3>Espectro de Peligro Uniforme y Espectro de Diseño</h3>
-			<span>
-				<h4>Periodo de retorno en años:</h4>
-				<input
-					type="number"
+  return (
+    <div className={style.uniformHazardContainer}>
+      <h3>Espectro de Peligro Uniforme y Espectro de Diseño</h3>
+      <span>
+        <h4>Periodo de retorno en años:</h4>
+        <input
+          type="number"
           value={returnPeriodInputted}
-					onChange={(event) => dispatch(inputReturnPeriod(event.target.value))}
-				/>
-				<button onClick={onSearch}>Calcular</button>
+          onChange={(event) => dispatch(inputReturnPeriod(event.target.value))}
+        />
+        <button onClick={onSearch}>Calcular</button>
 
-				{buttonsReturnPeriod.map((button) => (
-					<button
-						key={button}
-						onClick={() => dispatch(selectReturnPeriod(button.toString()))}
-						className={returnPeriodSelected[button] ? style.on : style.off}
-					>
-						{button}
-					</button>
-				))}
-			</span>
+        {buttonsReturnPeriod.map((button) => (
+          <button
+            key={button}
+            onClick={() => dispatch(selectReturnPeriod(button.toString()))}
+            className={returnPeriodSelected[button] ? style.on : style.off}
+          >
+            {button}
+          </button>
+        ))}
+      </span>
 
-			<span>
-				<h4>Periodos:</h4>
-				<select value={normSelected} name="selectNorm" onChange={handleChange}>
-					<option value="">Seleccionar norma</option>
-					{norms?.map((norm) => (
-						<option value={norm.value} key={norm.value}>
-							{norm.name}
-						</option>
-					))}
-				</select>
+      <span>
+        <h4>Periodos:</h4>
+        <select value={normSelected} name="selectNorm" onChange={handleChange}>
+          <option value="">Seleccionar norma</option>
+          {norms?.map((norm) => (
+            <option value={norm.value} key={norm.value}>
+              {norm.name}
+            </option>
+          ))}
+        </select>
 
-				<select value={soilSelected} name="selectSoil" onChange={handleChange}>
-					<option value="">Seleccionar tipo de suelo</option>
-					{soils?.map((soil) => (
-						<option value={soil.value} key={soil.value}>
-							{soil.name}
-						</option>
-					))}
-				</select>
-			</span>
+        <select value={soilSelected} name="selectSoil" onChange={handleChange}>
+          <option value="">Seleccionar tipo de suelo</option>
+          {soils?.map((soil) => (
+            <option value={soil.value} key={soil.value}>
+              {soil.name}
+            </option>
+          ))}
+        </select>
+      </span>
 
-			<br />
-			<DesignSpectrumChart/>
-		</div>
-	);
+      <br />
+      <div className={style.chartContainer}>
+        <DesignSpectrumChart />
+      </div>
+    </div>
+  );
 };
 
 export default UniformHazard;
