2b47cd12f806889775c48dbc4d3447bc84d117f5
Sebastian Satelier <sebastian.satelier@gmail.com>
2023-07-05 08:24:14 -0300

    feature add / fix errors / integration - add charts ExceedanceProbability and UniformHazard components to render; - fix requestToLocation; - add new property into globalState; - fix color error; - change function close in Map component.

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,54 +1,76 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-	location: { lat: -12.0, lng: -77.0 },
-	normSelected: "",
-	soilSelected: "",
-	norms: null,
-	soils: null,
-	periodSelected: {},
-	returnPeriodInputted: 0,
-	returnPeriodSelected: {},
+  location: {
+    id: "",
+    lat: "",
+    lng: "",
+    exceedanceProbability: {},
+    hazardSpectrum: {},
+    designSpectrum: [],
+  },
+  normSelected: "",
+  soilSelected: "",
+  norms: null,
+  soils: null,
+  periodSelected: {},
+  returnPeriodInputted: 0,
+  returnPeriodSelected: {},
 };
 
 export const slice = createSlice({
-	name: "globalState",
-	initialState,
-	reducers: {
-		setLocation: (state, { payload }) => {
-			state.location = payload;
-		},
-		setNorms: (state, { payload }) => {
-			state.norms = payload;
-		},
-		selectNorm: (state, { payload }) => {
-			state.normSelected = payload;
-			const norm = state.norms.find((norm) => norm.value === payload);
-			state.soils = norm?.soilType;
-			state.soilSelected = "";
-		},
-		selectSoil: (state, { payload }) => {
-			state.soilSelected = payload;
-		},
-		selectPeriod: (state, { payload }) => {
-			state.periodSelected[payload] = !state.periodSelected[payload];
-		},
-		inputReturnPeriod: (state, { payload }) => {
-			console.log(payload);
-			state.returnPeriodInputted = payload;
-		},
-		selectReturnPeriod: (state, { payload }) => {
-			state.returnPeriodSelected[payload] = !state.returnPeriodSelected[payload];
-		},
-	},
+  name: "globalState",
+  initialState,
+  reducers: {
+    setLocation: (state, { payload }) => {
+      state.location = {
+        ...state.location,
+        lat: payload.location.lat,
+        lng: payload.location.lng,
+        id: payload.data,
+      };
+    },
+    setNorms: (state, { payload }) => {
+      state.norms = payload;
+    },
+    selectNorm: (state, { payload }) => {
+      state.normSelected = payload;
+      const norm = state.norms.find((norm) => norm.value === payload);
+      state.soils = norm?.soilType;
+      state.soilSelected = "";
+    },
+    selectSoil: (state, { payload }) => {
+      state.soilSelected = payload;
+    },
+    selectPeriod: (state, { payload }) => {
+      state.periodSelected = payload;
+    },
+    selectDamping: (state, { payload }) => {
+      state.dampingSelected[payload] = !state.dampingSelected[payload];
+    },
+    inputReturnPeriod: (state, { payload }) => {
+      //console.log('inputReturnPeriod', payload);
+      state.returnPeriodInputted = payload;
+    },
+    selectReturnPeriod: (state, { payload }) => {
+      state.returnPeriodSelected[payload] =
+        !state.returnPeriodSelected[payload];
+    },
+    resultPeriod: (state, { payload }) => {
+      //console.log('resultPeriod ', payload)
+      state.location.exceedanceProbability[payload.period] = payload.data;
+    },
+  },
 });
 export const {
-	setLocation,
-	setNorms,
-	selectNorm,
-	selectSoil,
-	selectPeriod,
-	inputReturnPeriod,
-	selectReturnPeriod,
+  setLocation,
+  setNorms,
+  resultPeriod,
+  selectNorm,
+  selectSoil,
+  selectPeriod,
+  selectDamping,
+  inputReturnPeriod,
+  selectReturnPeriod,
 } = slice.actions;
 export default slice.reducer;
