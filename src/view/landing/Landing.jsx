75d63d59abf58d97c47fa8ed7cfa67d8995e8220
josejaramillo098@gmail.com <josejaramillo098@gmail.com>
2023-07-17 12:52:34 -0300

    Feature landing page

--- src/view/landing/Landing.jsx
+++ src/view/landing/Landing.jsx
@@ -1,12 +1,26 @@
+import Section from "../../components/landingInfo/Section";
 import style from "./Landing.module.css";
+import { titles, content } from "../../Helpers/texts";
 
 const Landing = () => {
-	return (
-		<div className={style.landingContainer}>
-			<br />
-			This is landing page
-		</div>
-	);
+  return (
+    <>
+      <div className={style.landingContainer}>
+        <header>
+          <h1>Peligro Sìsmico en el Perù</h1>
+          <p>
+            ¡Bienvenido a nuestra plataforma de cálculo y visualización de
+            probabilidades sísmicas en Perú!{" "}
+          </p>
+        </header>
+        <section>
+          <Section title={titles.objetives} content={content.objetives} />
+          <Section title={titles.function} content={content.function} />
+          <Section title={titles.getStarted} content={content.getStarted} />
+        </section>
+      </div>
+    </>
+  );
 };
 
 export default Landing;
