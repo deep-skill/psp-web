87ce8b0194e8a95891ef151455a3b01d2ba489fc
josejaramillo098@gmail.com <josejaramillo098@gmail.com>
2023-07-17 21:18:22 -0300

    Feature - map view to responsive

--- src/Helpers/texts.js
+++ src/Helpers/texts.js
@@ -1,11 +1,11 @@
 export const titles = {
     objetives: 'Objetivo',
-    function: '¿Como funciona?',
-    getStarted: '¿Como empiezo?'
+    function: '¿Cómo funciona?',
+    getStarted: '¿Cómo empiezo?'
 }
 
 export const content = {
-    objetives: 'Nuestro objetivo es brindarte información precisa y actualizada sobre las probabilidades de ocurrencia de sismos en diferentes regiones peruanas, junto con el espectro de peligro y el espectro de diseño correspondientes. Con estos datos fundamentales, podrás tomar decisiones informadas en el diseño y construcción de estructuras resilientes y seguras.',
-    function: 'Nuestra plataforma utiliza modelos y algoritmos avanzados, basados en datos geológicos y sísmicos confiables, para calcular las probabilidades sísmicas de manera precisa y detallada. Además, te ofrecemos una visualización clara y amigable, a través de gráficos interactivos, que te ayudarán a interpretar y analizar los resultados de manera eficiente.',
-    getStarted: 'Es tan sencillo como seleccionar una coordenada en el mapa, luego seleccionar criterios como periodos de retorno, tipos de normas o tipo de suelo segun sea tu caso y los graficos estaran disponibles automaticamente.'
+    objetives: 'Nuestro objetivo es brindarte información precisa y actualizada sobre las probabilidades de ocurrencia de sismos en diferentes regiones peruanas, junto con el espectro de peligro uniforme y el espectro de diseño correspondientes. Con estos datos fundamentales, podrás tomar decisiones informadas en el diseño y construcción de estructuras resilientes y seguras.',
+    function: 'Nuestra plataforma utiliza modelos y algoritmos avanzados basados en datos geológicos y sísmicos confiables, para calcular las probabilidades sísmicas de manera precisa y detallada. Además, te ofrecemos una visualización clara y amigable, a través de gráficos interactivos, que te ayudarán a interpretar y analizar los resultados de manera eficiente.',
+    getStarted: 'Es tan sencillo como seleccionar una coordenada en el mapa, luego seleccionar criterios como periodos de retorno, tipos de normas o tipo de suelo según sea tu caso y los gráficos estarán disponibles automáticamente.'
 }
